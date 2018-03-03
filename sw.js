d3cc65c620a209a1842b9bfc786029856e2a64de
ngitocat <qvunguyen@gmail.com>
2018-03-03 20:18:46 +0100

    11th commit

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 console.log("SW startup");
-self.addEventListener('fetch', event => event.respondWith(new Response('Hello from your friendly neighbourhood service worker!')))
+self.addEventListener('fetch', function(event) {event.respondWith(new Response('Hello from your friendly neighbourhood service worker!'))})
