5fa457023f322684b6023ad177244c7b4395f9e0
ngitocat <qvunguyen@gmail.com>
2018-03-03 20:25:28 +0100

    12th commit

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
 console.log("SW startup");
+self.addEventListener('install', function(event) {
+  console.log("SW installed");
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("SW activated");
+});
 self.addEventListener('fetch', function(event) {event.respondWith(new Response('Hello from your friendly neighbourhood service worker!'))})
