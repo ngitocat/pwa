018c05d56fae7457059973b37cc5d2cbd8cfd68f
ngitocat <34979405+ngitocat@users.noreply.github.com>
2018-09-25 00:57:10 +0200

    change url /sw.js to /pwa/sw.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register('/sw.js').then(registration => console.log("sw registered successful"))
+navigator.serviceWorker.register('/pwa/sw.js').then(registration => console.log("sw registered successful"))
 Notification.requestPermission().then(permission => navigator.serviceWorker.ready.then(reg => reg.showNotification("From brower")))
 var urlBase64ToUnit8Array = base64String => {
   var padding = "=".repeat((4 - base64String.length % 4) % 4);
